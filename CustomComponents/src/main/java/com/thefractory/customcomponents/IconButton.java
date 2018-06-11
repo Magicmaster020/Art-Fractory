ee29c2cf6404845a5a4a5050e37414e1f05d869d
FatMansKryptonite <38400385+FatMansKryptonite@users.noreply.github.com>
2018-06-12 01:33:38 +0200

    Reworked IconButton.
    To be able to access an Icon Pack in Scene Builder the the exported jar must first be compiled with this Icon Pack available. You can then choose the added Icon Pack by specifying its directory in Scene Builder

--- CustomComponents/src/main/java/com/thefractory/customcomponents/IconButton.java
+++ CustomComponents/src/main/java/com/thefractory/customcomponents/IconButton.java
@@ -1,5 +1,64 @@
 package com.thefractory.customcomponents;
 
-public class IconButton {
+import java.io.IOException;
+import javafx.beans.NamedArg;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
+//https://stackoverflow.com/questions/43956201/javafx-how-to-remove-the-borders-from-button
+
+public class IconButton extends Button {
+	
+    //Properties
+    private final StringProperty iconLocation;
+	
+	@FXML public Button button;
+	@FXML private Tooltip tip;
+	
+	public IconButton(@NamedArg(value="iconLocation" , defaultValue="closeButton") String iconLocation){
+		this.iconLocation = new SimpleStringProperty(this, "iconLocation", iconLocation);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("IconButton.fxml"));
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setClassLoader(getClass().getClassLoader());
+        
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        
+		button.getStylesheets().add(getClass().getResource("icons/" + iconLocation + "/style.css").toString());
+	}
+	
+	public void initialize() {
+	}
+	
+	public void function(){
+		System.out.println("Hej");
+	}
+	
+	public final String getIconLocation(){
+		return iconLocation.getValue();
+	}
+	public final void setIconLocation(String value){
+		iconLocation.setValue(value);
+	}
+	public final StringProperty iconLocationProperty(){
+		return iconLocation;
+	}
+	public final String getTip() {
+        return tip.getText();
+}
+	public final void setTip(String value) {
+	        tip.setText(value);
+	}
+	public final StringProperty tipProperty() {
+        return tip.textProperty();
+}
 }
---
+++ CustomComponents/src/main/java/com/thefractory/customcomponents/test/IconButtonTest.java
@@ -0,0 +1,31 @@
+package com.thefractory.customcomponents.test;
+
+import com.thefractory.customcomponents.IconButton;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class IconButtonTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Button iconButton = new IconButton("infoButton");
+
+        StackPane stack = new StackPane();
+        stack.getChildren().add(iconButton);
+        Scene scene = new Scene(stack);
+
+        primaryStage.setTitle("InfoIcon Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
