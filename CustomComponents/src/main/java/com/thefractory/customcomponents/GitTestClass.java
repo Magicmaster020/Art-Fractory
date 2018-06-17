42d2f805735bc8c7ccaa200efdb8e7d7f29df2c3
Isak <Isak@Kopus.lan>
2018-06-17 21:39:38 +0200

    Lite mer test

--- CustomComponents/src/main/java/com/thefractory/customcomponents/GitTestClass.java
+++ CustomComponents/src/main/java/com/thefractory/customcomponents/GitTestClass.java
@@ -3,5 +3,6 @@ package com.thefractory.customcomponents;
 public class GitTestClass {
 	public static void main(String[] args) {
 		System.out.println("Jak e bök");
+		System.out.println("hejhejhej");
     }
 }
