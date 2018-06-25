9475b62b0a3ba87a621b7d73e7ca7b2ac03648a6
Magicmaster020 <39906548+Magicmaster020@users.noreply.github.com>
2018-06-25 16:03:10 +0200

    Added test files for circular sliders

--- CustomComponents/src/main/java/com/thefractory/customcomponents/test/DoubleCircularSliderTest.java
+++ CustomComponents/src/main/java/com/thefractory/customcomponents/test/DoubleCircularSliderTest.java
@@ -1,23 +1,23 @@
 package com.thefractory.customcomponents.test;
 
-import com.thefractory.customcomponents.CircularSlider;
+import com.thefractory.customcomponents.DoubleCircularSlider;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class CircularSliderTest extends Application {
+public class DoubleCircularSliderTest extends Application {
 	
     @Override
     public void start(Stage primaryStage) {
     	
-		CircularSlider cslider = new CircularSlider(360,0);
+		DoubleCircularSlider cslider = new DoubleCircularSlider(360,0,2,0);
 		StackPane stack = new StackPane(cslider);
 		
         Scene scene = new Scene(stack);
 
-        primaryStage.setTitle("Circular Slider Test");
+        primaryStage.setTitle("Double Circular Slider Test");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
