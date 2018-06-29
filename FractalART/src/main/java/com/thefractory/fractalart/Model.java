932d10d1b84fd2c94a98e8e6e00104048b225e05
Isak <isak.kallman@gmail.com>
2018-06-29 14:30:28 +0200

    Save function update

--- FractalART/src/main/java/com/thefractory/fractalart/Model.java
+++ FractalART/src/main/java/com/thefractory/fractalart/Model.java
@@ -1,5 +1,6 @@
 package com.thefractory.fractalart;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -7,10 +8,12 @@ import javax.imageio.ImageIO;
 
 import com.thefractory.fractalart.test.ArtworkTest;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Stage;
 
 public class Model {
 	
@@ -27,21 +30,63 @@ public class Model {
 		
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Export Artwork");
+		fileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
 		fileChooser.getExtensionFilters().addAll(
-		         new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.tiff"));
+		         new ExtensionFilter("JPEG", "*.jpg", "*.jpeg", "*.jpe", "*.jfif"), 
+		         new ExtensionFilter("TIFF", "*.tif", "*.tiff"), 
+		         new ExtensionFilter("PNG", "*.png"), 
+		         new ExtensionFilter("GIF", "*.gif"));
 		//File outputfile = new File("images/" + artwork.getName() + "." + fileType);
-		File outputfile = fileChooser.showSaveDialog(ArtworkTest.primaryStage);
+		/*
+		new ChangeListener<ObjectProperty>() {
+			@Override
+			public void changed(ObservableValue<? extends ObjectProperty> observable, ObjectProperty oldValue,
+					ObjectProperty newValue) {
+				String name = fileChooser.getInitialFileName();
+				String extension = name.substring(1 + name.lastIndexOf(".")).toLowerCase();
+				System.out.println("Nu blev det " + newValue.getDescription());
+			}
+		});*/
 		
-		String fileType = "tiff";
 		
-		try {
-		switch (fileType) {
-			case "tiff": 
-				ImageIO.write(SwingFXUtils.fromFXImage(artwork.getImage(1920,1080), null), "png", outputfile);
-				System.out.println("Saved");
-		}
-		}catch(IOException e) {
-			System.out.println("Saving error");
+		File outputFile = fileChooser.showSaveDialog(ArtworkTest.primaryStage);
+		
+		if(outputFile != null) {
+			String name = outputFile.getName();
+		    String extension = name.substring(1 + name.lastIndexOf(".")).toLowerCase();
+		    System.out.println(extension);
+			try {
+			switch (extension) {
+				case "tiff":
+				case "tif":
+					System.out.println(ImageIO.write(SwingFXUtils.fromFXImage(artwork.getImage(1920,1080), null), "png", outputFile));
+					System.out.println("saved");
+					break;
+				case "png":
+					ImageIO.write(SwingFXUtils.fromFXImage(artwork.getImage(1920,1080), null), "png", outputFile);
+					System.out.println("saved");
+					break;
+				case "jpg":
+				case "jpeg":
+				case "jpe":
+				case "jfif":
+					BufferedImage bImage =SwingFXUtils.fromFXImage(artwork.getImage(1920,1080), null);
+					BufferedImage bImage2 = new BufferedImage(bImage.getWidth(), bImage.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+					bImage2.getGraphics().drawImage(bImage, 0, 0, null);
+					System.out.println(ImageIO.write(bImage2, "jpg", outputFile));
+					System.out.println("saved");
+					break;
+				case "gif":
+					ImageIO.write(SwingFXUtils.fromFXImage(artwork.getImage(1920,1080), null), "gif", outputFile);
+					System.out.println("saved");
+					break;
+				default: 
+					System.out.println("Unsupported extension");
+					break;
+			}
+			}catch(IOException e) {
+				System.out.println("Saving error");
+			}
 		}
 	}
 }
