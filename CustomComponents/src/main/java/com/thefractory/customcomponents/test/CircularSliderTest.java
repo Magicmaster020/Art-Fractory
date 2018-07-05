1bbb8799e175ed1eb3e700a96739f26d7a953575
Magicmaster020 <39906548+Magicmaster020@users.noreply.github.com>
2018-07-05 16:51:34 +0200

    Test file for CircularSlider

--- CustomComponents/src/main/java/com/thefractory/customcomponents/test/CircularSliderTest.java
+++ CustomComponents/src/main/java/com/thefractory/customcomponents/test/CircularSliderTest.java
@@ -1,8 +1,12 @@
 package com.thefractory.customcomponents.test;
 
+import javafx.beans.value.ChangeListener;
+
 import com.thefractory.customcomponents.CircularSlider;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -12,10 +16,20 @@ public class CircularSliderTest extends Application {
     @Override
     public void start(Stage primaryStage) {
     	
-		CircularSlider cslider = new CircularSlider(0,360,"M50,50 M50,0 A50,50 1 0,0 50,100 A50,50 1 0,0 50,0 z",true);
-		StackPane stack = new StackPane(cslider);
-		//"M50,50 M50,0 A50,50 1 0,1 50,100 A50,50 1 0,1 50,0 z"
+    	CircularSlider cslider = new CircularSlider(0, 360, "M50,0 A50,50 0 0 0 50,100 A50,50 0 0 0 50,0 z", true, 0);
+        CircularSlider cslider2 = new CircularSlider(0, 360, "M50,20 A30,30 0 0 0 50,80 A30,30 0 0 0 50,20 z", true, 0);
+        StackPane stack = new StackPane();
+        stack.getChildren().addAll(cslider2, cslider);
         Scene scene = new Scene(stack);
+
+        cslider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+                System.out.println(arg2);
+            }
+
+        });
 
         primaryStage.setTitle("Circular Slider Test");
         primaryStage.setScene(scene);
