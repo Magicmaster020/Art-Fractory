4f3e9353ba9556e9705b4424e711ce6760972e18
Ivar Eriksson <ivar.c.eriksson@hotmail.com>
2018-07-06 01:49:06 +0200

    Small edits.

--- FractalART/src/main/java/com/thefractory/fractalart/AbstractMandelbrotSet.java
+++ FractalART/src/main/java/com/thefractory/fractalart/AbstractMandelbrotSet.java
@@ -1,4 +1,5 @@
 package com.thefractory.fractalart;
+
 import com.thefractory.customcomponents.GradientPicker;
 import com.thefractory.fractalart.utils.ComplexNumber;
 
@@ -16,6 +17,7 @@ public abstract class AbstractMandelbrotSet extends Artwork {
 	protected IntegerProperty power = new SimpleIntegerProperty(2);
 	protected DoubleProperty realStart = new SimpleDoubleProperty(0.0);
 	protected DoubleProperty imaginaryStart = new SimpleDoubleProperty(0.0);
+	protected double hej = 50;
 	private GradientPicker gradientPicker;
 	
 	public AbstractMandelbrotSet(String name) {
@@ -29,7 +31,7 @@ public abstract class AbstractMandelbrotSet extends Artwork {
 	private int iterate(ComplexNumber c) {
 		ComplexNumber z = new ComplexNumber(realStart.get() , imaginaryStart.get());
 		int i = 0;
-		for(; i < iterations.get(); i++) {
+		for(; i < hej; i++) {
 			z = ComplexNumber.add(ComplexNumber.pow(z, power.get()), c);
 			if(Math.pow(z.getRe(), 2) + Math.pow(z.getIm(), 2) > 4) {
 				break;
@@ -47,26 +49,39 @@ public abstract class AbstractMandelbrotSet extends Artwork {
 	}
 	
 	@Override
-	public WritableImage getImage(int height, int width) {
+	public WritableImage getImage(int width, int height) {
 		double xLength = 1/rightPane.zoomField.getValue();
 		double[] center = {rightPane.xField.getValue(), rightPane.yField.getValue()};
 		double angle = -Math.toRadians(rightPane.angleField.getValue());
 		
-		WritableImage image = new WritableImage(height, width);
+		double minIterations = hej;
+		double[][] iterationsAt = new double[width][height];
+		WritableImage image = new WritableImage(width, height);
 		PixelWriter writer = image.getPixelWriter();
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
 				
 				ComplexNumber z = new ComplexNumber(
-						center[0] + xLength * (-Math.cos(angle) * (0.5 - ((double) i)/width) 
-								+ Math.sin(angle) * (0.5 - ((double) j)/width)), 
-						- center[1] + xLength * (-Math.sin(angle) * (0.5 - ((double) i)/width) 
-								- Math.cos(angle) * (0.5 - ((double) j)/width)));
-				
-				Color color = gradientPicker.getPalette().getColor(((double) iterate(z))/iterations.get());
+						center[0] + xLength * (-Math.cos(angle) * (((0.5 * width) - ((double) i))/width) 
+								+ Math.sin(angle) * (((0.5 * height) - ((double) j))/width)), 
+						- center[1] + xLength * (-Math.sin(angle) * (((0.5 * width) - ((double) i))/width) 
+								- Math.cos(angle) * (((0.5 * height) - ((double) j))/width)));
+				double tempIt = (double) iterate(z);
+				if (tempIt < minIterations){
+					minIterations = tempIt;
+				}
+				iterationsAt[i][j] = tempIt;
+			}
+		}
+		
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+				Color color = gradientPicker.getPalette().getColor((iterationsAt[i][j] - minIterations)/(hej - minIterations));
 				writer.setColor(i, j, color);
 			}
 		}
+		
+		hej = minIterations + iterations.get();
 		return(image);
 	}
 	
